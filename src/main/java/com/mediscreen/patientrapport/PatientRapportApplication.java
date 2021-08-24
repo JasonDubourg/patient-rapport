d14d7b1b1e9048e2d577d0550ca567ae4ef3140b
Jason <jasondubourg@gmail.com>
2021-08-24 10:47:16 +0200

    Test du controller

---
+++ src/main/java/com/mediscreen/patientrapport/PatientRapportApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.patientrapport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientRapportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientRapportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mediscreen/patientrapport/PatientRapportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.patientrapport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientRapportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
